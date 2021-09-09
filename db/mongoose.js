178fd179e3ac55c96bc20aeed8d8d15bee0ea4a1
jesseclldr <jesse.dragstra@redfigllc.com>
2021-09-09 14:17:22 +0200

    last one

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const url = process.env.URL || 'mongodb://127.0.0.1:27017/model-manager-api'
+const url = process.env.DB_URL || 'mongodb://127.0.0.1:27017/model-manager-api'
 mongoose 
  .connect(url, {
     useNewUrlParser:true,useUnifiedTopology: true  })   
